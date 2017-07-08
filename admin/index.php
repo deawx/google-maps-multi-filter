b60070c44241bf3828203ed0768ce2d9e7607140
Rhuan Carlos <rhcarlosweb@gmail.com>
2017-07-08 14:58:17 -0300

    initial admin client-side

---
+++ admin/index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Painel de Controle</title>
+
+    <!--font awesome-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+
+<div class="sidebar-dashboard">
+    <div class="logo-sidebar">
+        <a href="#"><img src="http://placehold.it/200x100" alt="logo"></a>
+    </div>
+    <!-- /.logo-sidebar -->
+
+    <div class="menu-sidebar">
+        <ul>
+            <li><a href="#"><i class="fa fa-home"></i> Painel de Controle</a></li>
+            <li><a href="#"><i class="fa fa-map"></i> Mapas</a></li>
+            <li><a href="#"><i class="fa fa-map-marker"></i> Marcadores Personalizados</a></li>
+        </ul>
+    </div>
+    <!-- /.menu-sidebar -->
+</div>
+<!-- /.sidebar-dashboard -->
+
+<div class="wrap-content-dashboard">
+
+    <header class="header-dashboard">
+        <div class="menu-dashboard">
+            <a href="#">
+                <i class="fa fa-map-marker"></i>
+                <span class="text-menu">
+                    Pontos
+                </span>
+                <!-- /.text-menu -->
+            </a>
+        </div>
+        <!-- /.menu-dashboard -->
+
+        <div class="buttons-header">
+            <a href="#">
+                <i class="fa fa-floppy-o"></i> Salvar
+            </a>
+        </div>
+        <!-- /.buttons-header -->
+    </header>
+    <!-- /.header-dashboard -->
+
+    <div class="content-dashboard">
+        Iframe <br>
+        Info box
+    </div>
+    <!-- /.content-dashboard -->
+
+    <div class="footer-dashboard">
+        Desenvolvido por <a href="http://www.rhuancarlos.com" target="_blank" title="Rhuan Carlos">Rhuan Carlos</a>
+    </div>
+    <!-- /.footer-dashboard -->
+
+</div>
+<!-- /.wrap-content-dashboard -->
+
+</body>
+</html>
